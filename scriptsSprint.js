94ddba57eb7e45055da3a2462e5331538d660a12
jonfervcHub <90652874+jonfervcHub@users.noreply.github.com>
2021-09-23 20:05:15 -0500

    cambios formularoi

---
+++ scriptsSprint.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//var document = "registro.html";
+
+
+
+
+console.log("prueba");
+/*function validar_nombre_usuario(nombre_usuario){
+
+ var tamaño_nombreUsuario = nombre_usuario.length;
+ 
+    console.log(tamaño_nombreUsuario);
+    
+    if(){
+        
+    }
+    
+}*/
