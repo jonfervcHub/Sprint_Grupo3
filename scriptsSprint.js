5c9bb36f13c2217f0b2cb2296de00264a5efed28
jonfervcHub <90652874+jonfervcHub@users.noreply.github.com>
2021-09-24 18:14:14 -0500

    arreglo registro

--- scriptsSprint.js
+++ scriptsSprint.js
@@ -1,13 +1,16 @@
 
 
 function validar_nombre_usuario(){
-    var nombreU = document.getElementById("in_nombre_usuario");
-    var cantidadCaracteres = nombreU.length;
-    console.log(cantidadCaracteres);
-    if (nombreU.length>2 ) {
-        console.log("cumple");
+    const nombreU = document.getElementById("in_nombre_usuario");
+    alert(nombreU.value);
+    const largo_nombreU = nombreU.value.length;
+    //nombreU.addEventListener("keypress");
+    //const cantidadCaracteres = nombreU.length;
+    //console.log(nombreU.value);
+    if (6 < largo_nombreU < 30) {
+        alert("En rango");
     } else {
-        console.log("no cumple")
+        alert("No en rango");
     }
 }
 /*var nombreU = document.getElementById("in_nombre_usuario");
